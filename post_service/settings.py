e4a28c6134aec573bd43701eee64f2b503290c5b
ADMIN <pt27122000@gmail.com>
2021-11-10 23:24:42 +0700

    update

--- post_service/settings.py
+++ post_service/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cHJpdmF0ZUtleS1jdVdlbGw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
