9a66e36034e0c1a04f954500f9b3d7508ec2dabf
ADMIN <pt27122000@gmail.com>
2021-11-30 21:59:09 +0700

    update api for cart

--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -1,10 +1,11 @@
+from apps.post import models as post_models
 from rest_framework import serializers
 from . import models
-from apps.post import models as post_models
 
 
 class PostImageSerializer(serializers.ModelSerializer):
     class Meta:
+        ref_name = "order_post_image"
         model = post_models.PostImage
         fields = ['id', 'url']
 
@@ -13,6 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
     images = PostImageSerializer(many=True)
 
     class Meta:
+        ref_name = "order_post"
         model = post_models.Post
         fields = ['title', 'price', 'description', 'images']
 
