a6e919823fb8c8b5acd9c7b21ad4a81e07233816
ADMIN <pt27122000@gmail.com>
2021-12-11 11:49:15 +0700

    add admin permission to statitis

--- core/authentication.py
+++ core/authentication.py
@@ -1,4 +1,5 @@
 from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import BasePermission
 from rest_framework import exceptions
 from django.conf import settings
 import jwt
@@ -8,21 +9,44 @@ def verify_token(token):
     try:
         payload = jwt.decode(token, key=settings.SECRET_KEY, algorithms=['HS256'], options={'verify_aud': False})
 
-        return payload['user']
+        return payload
+    except Exception as e:
+        return None
+
+
+def get_data_in_token(request):
+    try:
+        token = request.META.get('HTTP_AUTHORIZATION')
+        token = token.split(' ')[1]
+
+        data = verify_token(token)
+        return data
+
     except Exception as e:
         return None
 
 
 class Authentication(BaseAuthentication):
     def authenticate(self, request):
-        try:
-            token = request.META.get('HTTP_AUTHORIZATION')
-            token = token.split(' ')[1]
+        data = get_data_in_token(request)
 
-            user = verify_token(token)
-            if not user:
-                raise exceptions.AuthenticationFailed('token is invalid')
-
-            return user, None
-        except Exception as e:
+        if not data:
             raise exceptions.AuthenticationFailed('token is invalid')
+
+        return data['user']
+
+
+class AdminPermission(BasePermission):
+    def has_permission(self, request, view):
+        data = get_data_in_token(request)
+
+        if not data:
+            raise exceptions.AuthenticationFailed('token is invalid')
+
+        role = data['role']
+
+        if 'Admin' in role or 'admin' in role:
+            return True
+
+        raise exceptions.AuthenticationFailed('you must have admin permission to perform this action')
+
